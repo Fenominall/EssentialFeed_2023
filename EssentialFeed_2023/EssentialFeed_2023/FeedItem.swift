41d59fdb626c6cdc92e9848378248ec18faee1aa
Vladislav Todorov <it.vtodorov@gmail.com>
2023-02-21 16:42:00 +0300

    Create EssentialFeed_2023 framework

---
+++ EssentialFeed_2023/EssentialFeed_2023/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed_2023
+//
+//  Created by Fenominall on 2/21/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let descreption: String?
+    let location: String?
+    let imageURL: URL
+}
