0588cc0c4e6f66a75edb17fc13eff50aae643e36
Vladislav Todorov <it.vtodorov@gmail.com>
2023-02-26 12:04:38 +0300

    Create factory methods to simplify test setup

--- EssentialFeed_2023/EssentialFeed_2023Tests/RemoteFeedLoaderTests.swift
+++ EssentialFeed_2023/EssentialFeed_2023Tests/RemoteFeedLoaderTests.swift
@@ -9,7 +9,7 @@ import XCTest
 import EssentialFeed_2023
 
 final class RemoteFeedLoaderTests: XCTestCase {
-
+    
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
         XCTAssertTrue(client.requesterURLs.isEmpty)
@@ -34,7 +34,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         
         XCTAssertEqual(client.requesterURLs, [url, url])
     }
-
+    
     // MARK: - Checking Error Courses
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
@@ -65,7 +65,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
             client.complete(withStatusCode: 200, data: invalidJSON)
         }
     }
-
+    
     // MARK: - Checking Success Courses
     func test_load_deliversNoItemsOn200HTTPResponseWithEmptyList() {
         let (sut, client) = makeSUT()
@@ -74,7 +74,26 @@ final class RemoteFeedLoaderTests: XCTestCase {
             client.complete(withStatusCode: 200, data: emptyListJSON)
         }
     }
-
+    
+    func test_load_deliverFeedItemsOn200HTTPResponseWithValidJSONItems() {
+        let (sut, client) = makeSUT()
+        
+        let item1 = makeItem(
+            id: UUID(),
+            imageURL: URL(string: "https://a-url.com")!)
+        let item2 = makeItem(
+            id: UUID(),
+            description: "a descreption",
+            location: "a location",
+            imageURL: URL(string: "https://another-url.com")!)
+        let itemModels = [item1.model, item2.model]
+        
+        expect(sut, toCompleteWithResult: .success(itemModels)) {
+            let json = makeItemsJSON([item1.json, item2.json])
+            client.complete(withStatusCode: 200, data: json)
+        }
+    }
+    
     // MARK: - Helpers
     // It`s better to implement the Spy by just capturing the values
     private class HTTPClientSpy: HTTPClient {
@@ -111,6 +130,36 @@ final class RemoteFeedLoaderTests: XCTestCase {
         return (sut, client)
     }
     
+    private func makeItem(
+        id: UUID,
+        description: String? = nil,
+        location: String? = nil,
+        imageURL: URL) ->
+    (model: FeedItem, json: [String: Any]) {
+        let item = FeedItem(
+            id: id,
+            description: description,
+            location: location,
+            imageURL: imageURL)
+        // To avoid typecasting json to as [String : Any] we can use reduce()
+        let json = [
+            "id": id.uuidString,
+            "description": description,
+            "location": location,
+            "image": imageURL.absoluteString
+        ].reduce(into: [String: Any]()) { (acc, element) in
+            // If we have a value the element it`s added to the dictionary - acc
+            if let value = element.value { acc[element.key] = value }
+        }
+        
+        return (item, json )
+    }
+    
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        let json = ["items": items]
+        return try! JSONSerialization.data(withJSONObject: json)
+    }
+    
     private func expect(
         _ sut: RemoteFeedLoader,
         toCompleteWithResult result: RemoteFeedLoader.Results,
@@ -119,11 +168,11 @@ final class RemoteFeedLoaderTests: XCTestCase {
         line: UInt = #line) {
             var capturedResults = [RemoteFeedLoader.Results]()
             sut.load { capturedResults.append($0) }
-                
+            
             action()
             
             XCTAssertEqual(capturedResults, [result], file: file, line: line)
-    }
+        }
     
     private func makeURL(_ url: URL
                          = URL(string: "https://a-url.com")!) -> URL {
