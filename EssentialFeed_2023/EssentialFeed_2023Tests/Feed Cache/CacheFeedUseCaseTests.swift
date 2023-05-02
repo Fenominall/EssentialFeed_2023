00cb525da75ae27940fb4a50a933973fdbf85e55
Vladislav Todorov <it.vtodorov@gmail.com>
2023-05-02 13:29:28 +0300

    Save command fails on cache insertion error

--- EssentialFeed_2023/EssentialFeed_2023Tests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed_2023/EssentialFeed_2023Tests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -19,9 +19,12 @@ class LocalFeedLoader {
     
     func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
         store.deleteCachedFeed { [unowned self] error in
-            completion(error)
             if error == nil {
-                self.store.insert(items, timestamp: self.currentDate())
+                self.store.insert(
+                    items, timestamp: self.currentDate(),
+                    completion: completion)
+            } else {
+                completion(error)
             }
         }
     }
@@ -31,7 +34,7 @@ class LocalFeedLoader {
 /// To decouple the application from framework details, we dont let frameworks dicatte the Use Case interfaces (e.g., adding Codable requirements or CoreData managed contexts oarameters). We do so by test-driving the interfaces the Use case needs for its collaborators, rather than defining the interface upfront to facilitate a specific framework implementation.`
 class FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
-    private var deletionCompletions = [DeletionCompletion]()
+    typealias InsertionCompletion = (Error?) -> Void
     
     // Unsing enum for testing to guarantee ate the same time which messages were invoiked with which values and in which order
     enum ReceivedMessages: Equatable {
@@ -41,12 +44,16 @@ class FeedStore {
     
     private (set) var receivedMessages = [ReceivedMessages]()
     
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         deletionCompletions.append(completion)
         receivedMessages.append(.deleteCachedFeed)
     }
     
-    func insert(_ items: [FeedItem], timestamp: Date) {
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
         receivedMessages.append(.inser(items, timestamp))
     }
     
@@ -55,7 +62,11 @@ class FeedStore {
     }
     
     func completeDeletionSuccessfully(at index: Int = 0) {
-         deletionCompletions[index](nil)
+        deletionCompletions[index](nil)
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
     }
 }
 
@@ -87,7 +98,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed])
     }
-
+    
     func test_save_requestsNewCacheInsertionWithTimeStampOnSuccessfullDeletion() {
         let timestamp = Date()
         let items = [uniqueItem(), uniqueItem()]
@@ -99,7 +110,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .inser(items, timestamp)])
     }
     
-    func test_save_failsCacheInsertionOnDeletionError() {
+    func test_save_failsOnDeletionError() {
         let (sut,store) = makeSUT()
         let items = [uniqueItem(), uniqueItem()]
         let deletionError = anyNSError()
@@ -116,7 +127,23 @@ final class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(receivedError as NSError?, deletionError)
     }
     
-    
+    func test_save_failsOnInsertionError() {
+        let (sut,store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        let insertionError = anyNSError()
+        let exp = expectation(description: "Wait for save cimpletion")
+        
+        var receivedError: Error?
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeDeletionSuccessfully()
+        store.completeInsertion(with: insertionError)
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedError as NSError?, insertionError)
+    }
     
     // MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,
