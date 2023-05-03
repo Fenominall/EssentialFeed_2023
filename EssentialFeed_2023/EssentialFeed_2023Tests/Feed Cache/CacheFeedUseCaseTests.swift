f4c342a4c187cf296fd02f22c98c9ad518fd4955
Vladislav Todorov <it.vtodorov@gmail.com>
2023-05-03 13:26:25 +0300

    Extract ''FeedStore' protocol from test spy helper

--- EssentialFeed_2023/EssentialFeed_2023Tests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed_2023/EssentialFeed_2023Tests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -30,50 +30,13 @@ class LocalFeedLoader {
     }
 }
 
-/// The FeedStore is a helper class representing the framework side to help us define the Use Case needs for its collaborator, making sure not to leak framework details into the Use Case.
-/// To decouple the application from framework details, we dont let frameworks dicatte the Use Case interfaces (e.g., adding Codable requirements or CoreData managed contexts oarameters). We do so by test-driving the interfaces the Use case needs for its collaborators, rather than defining the interface upfront to facilitate a specific framework implementation.`
-class FeedStore {
+protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
     
-    // Unsing enum for testing to guarantee ate the same time which messages were invoiked with which values and in which order
-    enum ReceivedMessages: Equatable {
-        case deleteCachedFeed
-        case inser([FeedItem], Date)
-    }
-    
-    private (set) var receivedMessages = [ReceivedMessages]()
-    
-    private var deletionCompletions = [DeletionCompletion]()
-    private var insertionCompletions = [InsertionCompletion]()
-    
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        deletionCompletions.append(completion)
-        receivedMessages.append(.deleteCachedFeed)
-    }
-    
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
-        insertionCompletions.append(completion)
-        receivedMessages.append(.inser(items, timestamp))
-    }
-    
-    func completeDeletion(with error: Error, at index: Int = 0) {
-        deletionCompletions[index](error)
-    }
-    
-    func completeDeletionSuccessfully(at index: Int = 0) {
-        deletionCompletions[index](nil)
-    }
-    
-    func completeInsertion(with error: Error, at index: Int = 0) {
-        insertionCompletions[index](error)
-    }
-    
-    func completeInsertionSuccessfully(at index: Int = 0) {
-        insertionCompletions[index](nil)
-    }
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
 }
-
 
 final class CacheFeedUseCaseTests: XCTestCase {
     
@@ -145,8 +108,8 @@ final class CacheFeedUseCaseTests: XCTestCase {
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,
                          file: StaticString = #filePath,
                          line: UInt = #line)
-    -> (sut: LocalFeedLoader, store: FeedStore) {
-        let store = FeedStore()
+    -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
         let sut = LocalFeedLoader(store: store, currentDate: currentDate)
         trackForMemoryLeak(store, file: file, line: line)
         trackForMemoryLeak(sut, file: file, line: line)
@@ -178,4 +141,49 @@ final class CacheFeedUseCaseTests: XCTestCase {
                  location: "location",
                  imageURL: anyURL())
     }
+    
+    /// The FeedStore is a helper class representing the framework side to help us define the Use Case needs for its collaborator, making sure not to leak framework details into the Use Case.
+    /// To decouple the application from framework details, we dont let frameworks dicatte the Use Case interfaces (e.g., adding Codable requirements or CoreData managed contexts oarameters). We do so by test-driving the interfaces the Use case needs for its collaborators, rather than defining the interface upfront to facilitate a specific framework implementation.`
+    private class FeedStoreSpy: FeedStore {
+        
+        // Unsing enum for testing to guarantee ate the same time which messages were invoiked with which values and in which order
+        enum ReceivedMessages: Equatable {
+            case deleteCachedFeed
+            case inser([FeedItem], Date)
+        }
+        
+        private (set) var receivedMessages = [ReceivedMessages]()
+        
+        private var deletionCompletions = [DeletionCompletion]()
+        private var insertionCompletions = [InsertionCompletion]()
+        
+        func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+            deletionCompletions.append(completion)
+            receivedMessages.append(.deleteCachedFeed)
+        }
+        
+        func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+            insertionCompletions.append(completion)
+            receivedMessages.append(.inser(items, timestamp))
+        }
+        
+        func completeDeletion(with error: Error, at index: Int = 0) {
+            deletionCompletions[index](error)
+        }
+        
+        func completeDeletionSuccessfully(at index: Int = 0) {
+            deletionCompletions[index](nil)
+        }
+        
+        func completeInsertion(with error: Error, at index: Int = 0) {
+            insertionCompletions[index](error)
+        }
+        
+        func completeInsertionSuccessfully(at index: Int = 0) {
+            insertionCompletions[index](nil)
+        }
+    }
+
+
+
 }
