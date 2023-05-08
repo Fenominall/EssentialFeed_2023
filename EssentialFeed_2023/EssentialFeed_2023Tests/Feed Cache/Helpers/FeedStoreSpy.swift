8e6af634b9573dad4f9f40fae9c0368739bed761
Vladislav Todorov <it.vtodorov@gmail.com>
2023-05-08 15:13:13 +0300

    Extract FeedCacheS[y into a shared scope to remove duplication

---
+++ EssentialFeed_2023/EssentialFeed_2023Tests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,52 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeed_2023Tests
+//
+//  Created by Fenominall on 5/8/23.
+//
+
+import Foundation
+import EssentialFeed_2023
+
+
+// The FeedStore is a helper class representing the framework side to help us define the Use Case needs for its collaborator, making sure not to leak framework details into the Use Case.
+// To decouple the application from framework details, we dont let frameworks dicatte the Use Case interfaces (e.g., adding Codable requirements or CoreData managed contexts oarameters). We do so by test-driving the interfaces the Use case needs for its collaborators, rather than defining the interface upfront to facilitate a specific framework implementation.`
+class FeedStoreSpy: FeedStore {
+    
+    // Unsing enum for testing to guarantee ate the same time which messages were invoiked with which values and in which order
+    enum ReceivedMessages: Equatable {
+        case deleteCachedFeed
+        case inser([LocalFeedImage], Date)
+    }
+    
+    private (set) var receivedMessages = [ReceivedMessages]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCachedFeed)
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.inser(feed, timestamp))
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+}
--- EssentialFeed_2023/EssentialFeed_2023Tests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed_2023/EssentialFeed_2023Tests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -27,44 +27,5 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         trackForMemoryLeak(sut, file: file, line: line)
         return (sut, store)
     }
-    
-    private class FeedStoreSpy: FeedStore {
-        
-        // Unsing enum for testing to guarantee ate the same time which messages were invoiked with which values and in which order
-        enum ReceivedMessages: Equatable {
-            case deleteCachedFeed
-            case inser([LocalFeedImage], Date)
-        }
-        
-        private (set) var receivedMessages = [ReceivedMessages]()
-        
-        private var deletionCompletions = [DeletionCompletion]()
-        private var insertionCompletions = [InsertionCompletion]()
-        
-        func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-            deletionCompletions.append(completion)
-            receivedMessages.append(.deleteCachedFeed)
-        }
-        
-        func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-            insertionCompletions.append(completion)
-            receivedMessages.append(.inser(feed, timestamp))
-        }
-        
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-        
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-        
-        func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-    }
+
 }
