cb30273a80dc99dd060be850d4e93f0b611a2f26
Vladislav Todorov <it.vtodorov@gmail.com>
2023-05-10 15:34:02 +0300

    Move "FeedCachePolicy' to its pwn file

--- EssentialFeed_2023/EssentialFeed_2023/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed_2023/EssentialFeed_2023/FeedCache/LocalFeedLoader.swift
@@ -7,26 +7,6 @@
 
 import Foundation
 
-// Business rules can be extracted to it`s own model and be reused across applications later
-private final class FeedCachePolicy {
-    private init() {}
-    
-    private static let calendar = Calendar(identifier: .gregorian)
-    
-    private static var maxCacheAge: Int {
-        7
-    }
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let maxCacheAge = calendar
-            .date(byAdding: .day, value: maxCacheAge, to: timestamp) else {
-            return false
-        }
-        return date < maxCacheAge
-    }
-}
-
-
 // Controller Boundary - Holds application specific logic
 public class LocalFeedLoader {
     private let store: FeedStore
