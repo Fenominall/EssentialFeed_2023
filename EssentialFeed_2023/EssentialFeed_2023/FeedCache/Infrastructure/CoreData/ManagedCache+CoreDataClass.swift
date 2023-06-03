9ee3bea2389e0260f58ea8c7346680592f205485
Vladislav Todorov <it.vtodorov@gmail.com>
2023-06-03 09:33:08 +0300

    Add ManagedCache and ManagedFeedImage model representations

---
+++ EssentialFeed_2023/EssentialFeed_2023/FeedCache/Infrastructure/CoreData/ManagedCache+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ManagedCache+CoreDataClass.swift
+//  EssentialFeed_2023
+//
+//  Created by Fenominall on 6/3/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedCache)
+public class ManagedCache: NSManagedObject {
+
+}
