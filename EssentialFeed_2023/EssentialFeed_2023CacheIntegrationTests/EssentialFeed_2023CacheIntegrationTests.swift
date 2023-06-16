bddd932b3c7dfdc94fef06f12857c68ae733aafb
Vladislav Todorov <it.vtodorov@gmail.com>
2023-06-16 17:57:33 +0300

    Extract delicate cache save expectations into a shared test helper

--- EssentialFeed_2023/EssentialFeed_2023CacheIntegrationTests/EssentialFeed_2023CacheIntegrationTests.swift
+++ EssentialFeed_2023/EssentialFeed_2023CacheIntegrationTests/EssentialFeed_2023CacheIntegrationTests.swift
@@ -21,7 +21,7 @@ final class EssentialFeed_2023CacheIntegrationTests: XCTestCase {
     
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
-            
+        
         expect(sut, toLoad: [])
     }
     
@@ -30,13 +30,8 @@ final class EssentialFeed_2023CacheIntegrationTests: XCTestCase {
         let sutToPerformLoad = makeSUT()
         let feed = uniqueImageFeed().models
         
-        let saveExp = expectation(description: "Wait for save completion")
-        sutToPerformSave.save(feed) { saveError in
-            XCTAssertNil(saveError, "Expected to save fedd successfully")
-            saveExp.fulfill()
-        }
-        wait(for: [saveExp], timeout: 1.0)
-
+        save(feed, with: sutToPerformSave)
+        
         expect(sutToPerformLoad, toLoad: feed)
     }
     
@@ -44,23 +39,12 @@ final class EssentialFeed_2023CacheIntegrationTests: XCTestCase {
         let sutToPerformFirstSave = makeSUT()
         let sutToPerformLastSave = makeSUT()
         let sutToPerformLoad = makeSUT()
-
+        
         let firstFeed = uniqueImageFeed().models
         let latestFeed = uniqueImageFeed().models
-
-        let saveExp1 = expectation(description: "Wait for save completion")
-        sutToPerformFirstSave.save(firstFeed) { saveError in
-            XCTAssertNil(saveError, "Expected to save fedd successfully")
-            saveExp1.fulfill()
-        }
-        wait(for: [saveExp1], timeout: 1.0)
-
-        let saveExp2 = expectation(description: "Wait for save completion")
-        sutToPerformLastSave.save(latestFeed) { saveError in
-            XCTAssertNil(saveError, "Expected to save fedd successfully")
-            saveExp2.fulfill()
-        }
-        wait(for: [saveExp2], timeout: 1.0)
+        
+        save(firstFeed, with: sutToPerformFirstSave)
+        save(latestFeed, with: sutToPerformLastSave)
 
         expect(sutToPerformLoad, toLoad: latestFeed)
     }
@@ -83,18 +67,32 @@ final class EssentialFeed_2023CacheIntegrationTests: XCTestCase {
         file: StaticString = #file,
         line: UInt = #line) {
             
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { result in
-            switch result {
-            case let .success(loadedFeed):
-                XCTAssertEqual(expectedFeed, loadedFeed, "Expected empty feed")
-            case let .failure(error):
-                XCTFail("Expected successfull feed result, got \(error) instead")
+            let exp = expectation(description: "Wait for load completion")
+            sut.load { result in
+                switch result {
+                case let .success(loadedFeed):
+                    XCTAssertEqual(expectedFeed, loadedFeed, "Expected empty feed")
+                case let .failure(error):
+                    XCTFail("Expected successfull feed result, got \(error) instead")
+                }
+                exp.fulfill()
             }
-            exp.fulfill()
+            wait(for: [exp], timeout: 1.0)
         }
-        wait(for: [exp], timeout: 1.0)
-    }
+    
+    private func save(
+        _ feed: [FeedImage],
+        with loader: LocalFeedLoader,
+        file: StaticString = #file,
+        line: UInt = #line) {
+            let saveExp = expectation(description: "Wait for save completion")
+            loader.save(feed) { saveError in
+                XCTAssertNil(saveError, "Expected to save fedd successfully")
+                saveExp.fulfill()
+            }
+            wait(for: [saveExp], timeout: 1.0)
+            
+        }
     
     private func testSpecificStoreURL() -> URL {
         return cachesDirectory().appending(path: "\(type(of: self)).store")
