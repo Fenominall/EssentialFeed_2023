b883faba3239fecd4832d048db0f77307e03cc63
Vladislav Todorov <it.vtodorov@gmail.com>
2023-06-25 16:07:58 +0300

    Render production feed data on the table view

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,25 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Fenominall on 6/25/23.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    
+}
