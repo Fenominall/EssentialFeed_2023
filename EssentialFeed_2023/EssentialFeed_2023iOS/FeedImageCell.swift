08117ce91dd79f7da20bcdee4aeb875c2277fc53
Vladislav Todorov <it.vtodorov@gmail.com>
2023-07-19 18:06:53 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeed_2023/EssentialFeed_2023iOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeed_2023iOS
+//
+//  Created by Fenominall on 7/19/23.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
