031dfa1ca575961f2a7f7bc361a9ad13cb4705fd
Vladislav Todorov <it.vtodorov@gmail.com>
2023-07-23 16:46:04 +0300

    preload image URL when image view is near visible

--- EssentialFeed_2023/EssentialFeed_2023iOS/FeedViewController.swift
+++ EssentialFeed_2023/EssentialFeed_2023iOS/FeedViewController.swift
@@ -19,7 +19,7 @@ public protocol FeedImageDataLoader {
                        completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
 }
 
-public final class FeedViewController: UITableViewController {
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     
     private var feedLoader: FeedLoader?
     private var imageLoader: FeedImageDataLoader?
@@ -37,6 +37,7 @@ public final class FeedViewController: UITableViewController {
         
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        tableView.prefetchDataSource = self
         load()
     }
     
@@ -86,5 +87,12 @@ public final class FeedViewController: UITableViewController {
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         tasks[indexPath]?.cancel()
         tasks[indexPath] = nil
+    }
+    
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        indexPaths.forEach { indexPath in
+            let cellModel = tableModel[indexPath.row]
+            _ = imageLoader?.loadImageData(from: cellModel.url) { _ in }
+        }
     }
 }
