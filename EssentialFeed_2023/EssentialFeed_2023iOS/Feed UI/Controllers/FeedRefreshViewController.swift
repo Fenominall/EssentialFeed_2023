a937072930f6c5c1ef842cb8e50abd28070f6fa2
Vladislav Todorov <it.vtodorov@gmail.com>
2023-10-18 21:08:03 +0300

    Remove mutable state from 'FeedViewModel'. The 'FeedViewModel' only needs to forward state changes, so state is only transient.

--- EssentialFeed_2023/EssentialFeed_2023iOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed_2023/EssentialFeed_2023iOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -22,12 +22,12 @@ final class FeedRefreshViewController: NSObject {
     }
     
     private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
-        // onChange closure is the binding logic between the viewModel and the View
-        viewModel.onChange = { [weak self] viewModel in
-            if viewModel.isLoading {
-                self?.view.beginRefreshing()
+        // onLoadingStateChange closure is the binding logic between the viewModel and the View
+        viewModel.onLoadingStateChange = { [weak view] isLoading in
+            if isLoading {
+                view?.beginRefreshing()
             } else {
-                self?.view.endRefreshing()
+                view?.endRefreshing()
             }
         }
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
--- EssentialFeed_2023/EssentialFeed_2023iOS/Feed UI/Models/FeedViewModel.swift
+++ EssentialFeed_2023/EssentialFeed_2023iOS/Feed UI/Models/FeedViewModel.swift
@@ -8,28 +8,26 @@
 import EssentialFeed_2023
 
 final class FeedViewModel {
+    typealias Observer<T> = (T) -> Void
+    
     private let feedLoader: FeedLoader
     
     init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
     }
     
-    // Returns a reference of the FeedViewModel
-    var onChange: ((FeedViewModel) -> Void)?
-    // The way to send the result outside of the clise via using a closure
-    var onFeedLoad: (([FeedImage]) -> Void)?
-    
-    private(set) var isLoading: Bool = false {
-        didSet { onChange?(self) }
-    }
-    
+    // Closure observers for states
+    var onLoadingStateChange: Observer<Bool>?
+    // The way to send the result outside of the class via using a closure
+    var onFeedLoad: Observer<[FeedImage]>?
+
     func loadFeed() {
-        isLoading = true
+        onLoadingStateChange?(true)
         feedLoader.load { [weak self] result in
             if let feed = try? result.get() {
                 self?.onFeedLoad?(feed)
             }
-            self?.isLoading = false
+            self?.onLoadingStateChange?(false)
         }
     }
 }
