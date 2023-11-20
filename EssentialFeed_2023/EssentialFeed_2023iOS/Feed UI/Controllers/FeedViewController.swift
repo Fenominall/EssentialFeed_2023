2453b0da7b80ad08e77948b4a84c9470d4c3139f
Vladislav Todorov <it.vtodorov@gmail.com>
2023-11-20 13:50:20 +0300

    Reuse table view cells from storyboard configuration

--- EssentialFeed_2023/EssentialFeed_2023iOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed_2023/EssentialFeed_2023iOS/Feed UI/Controllers/FeedViewController.swift
@@ -42,7 +42,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        cellControll(forRowAt: indexPath).view()
+        cellControll(forRowAt: indexPath).view(in: tableView)
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
