50b54a5ab716eb247c2970240e34cfdf9ed92353
Vladislav Todorov <it.vtodorov@gmail.com>
2023-12-03 21:35:29 +0300

    'RemoteFeedImageDataLoader.loadImageDataFromURL' delivers error on client error

--- EssentialFeed_2023/EssentialFeed_2023Tests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed_2023/EssentialFeed_2023Tests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
@@ -15,9 +15,14 @@ final class RemoteFeedImageDataLoader {
         self.client = client
     }
     
-    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
-        client.get(from: url) { _ in
-            
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result
+    ) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+            case let .failure(error):
+                completion(.failure(error))
+            default: break
+            }
         }
     }
 }
@@ -48,8 +53,17 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         sut.loadImageData(from: url) { _ in }
         
         XCTAssertEqual(client.requesterURLs, [url, url])
-
     }
+    
+    func test_loadImageDataFromURL_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        let clientError = anyNSError()
+        
+        expect(sut, toCompleteWith: .failure(clientError)) {
+            client.complete(with: clientError)
+        }
+    }
+    
     
     // MARK: - Helpers
     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClinetSpy) {
@@ -59,14 +73,47 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         trackForMemoryLeaks(presenter, file: file, line: line)
         return (presenter, client)
     }
+    
+    private func expect(
+        _ sut: RemoteFeedImageDataLoader,
+        toCompleteWith expectedResult: FeedImageDataLoader.Result,
+        when action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line) {
+            let url = URL(string: "https://a-given-url.com")!
+            let exp = expectation(description: "Wait for load completion")
+            
+            sut.loadImageData(from: url) { receivedResult in
+                switch (receivedResult, expectedResult) {
+                case let (.success(receivedData), .success(expectedData)):
+                    XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+                    
+                case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                    XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                default:
+                    XCTFail("Expected result \(expectedResult) got \(receivedResult) insted", file: file, line: line)
+                }
+                exp.fulfill()
+            }
+            
+            action()
+            wait(for: [exp], timeout: 2.0)
+        }
+    
         
     private final class HTTPClinetSpy: HTTPClient {
-        var requesterURLs = [URL]()
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-            requesterURLs.append(url)
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+        var requesterURLs: [URL] {
+            return messages.map { $0.url }
         }
         
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            messages.append((url, completion))
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
     }
 }
 
