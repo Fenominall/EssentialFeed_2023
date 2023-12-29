3beea77b7bb0af58a2bc001d69848e56f0348a9a
Vladislav Todorov <it.vtodorov@gmail.com>
2023-12-29 15:54:45 -0500

    Move RemoteFeedImageDataLoader to production

--- EssentialFeed_2023/EssentialFeed_2023Tests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed_2023/EssentialFeed_2023Tests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
@@ -8,63 +8,6 @@
 import EssentialFeed_2023
 import XCTest
 
-final class RemoteFeedImageDataLoader {
-    let client: HTTPClient
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    public enum Error: Swift.Error {
-        case invalidData
-    }
-    
-    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        
-        var wrapperd: HTTPClientTask?
-        
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-            wrapperd?.cancel()
-        }
-        
-        func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-    
-    @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result
-    ) -> Void) -> FeedImageDataLoaderTask {
-        // The result passed in task.complere(with:) will be passed from the HTTPClientTaskWrapper to the completion of loadImageData() because completion in the class is the reference type
-        let task = HTTPClientTaskWrapper(completion)
-        task.wrapperd = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case let .failure(error): task.complete(with: .failure(error))
-            }
-        }
-        return task
-    }
-}
-
-
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
     
     func test_init_doesNotPerformAnyURLRequest() {
