3f549e89f6c2a0866e71eb8bd94da5d3a7d89fe5
Vladislav Todorov <it.vtodorov@gmail.com>
2024-01-04 12:14:30 -0500

    Move 'CoreDataFeedStore' implementation of the 'FeedImageDataStore' protocol to production

---
+++ EssentialFeed_2023/EssentialFeed_2023/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataFeedStore+FeedImageDataLoader.swift
+//  EssentialFeed_2023
+//
+//  Created by Fenominall on 04.01.2024.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        
+    }
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
