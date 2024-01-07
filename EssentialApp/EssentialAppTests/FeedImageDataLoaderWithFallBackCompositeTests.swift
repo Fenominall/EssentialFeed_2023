f24aeff1116a49c466ad5118f43a32f4c153b1fa
Vladislav Todorov <it.vtodorov@gmail.com>
2024-01-07 14:06:23 -0500

    'FeedImageDataLoaderWithFallbackComposite.init' does not load image data

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallBackCompositeTests.swift
@@ -0,0 +1,60 @@
+//
+//  FeedImageDataLoaderWithFallBackCompositeTests.swift
+//  EssentialAppTests
+//
+//  Created by Fenominall on 07.01.2024.
+//
+
+import XCTest
+import EssentialFeed_2023
+
+class FeedImageDataLoaderWithFallBackComposite: FeedImageDataLoader {
+    private let primary: FeedImageDataLoader
+    private let fallback: FeedImageDataLoader
+    
+    private class Task: FeedImageDataLoaderTask {
+        func cancel() {
+            
+        }
+    }
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return Task()
+    }
+}
+
+class FeedImageDataLoaderWithFallBackCompositeTests: XCTestCase {
+    func test_init_doesNotLoadImageData() {
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        _ = FeedImageDataLoaderWithFallBackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+
+    }
+    
+    // MARK: - Helpers
+    private class LoaderSpy: FeedImageDataLoader {
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        private class Task: FeedImageDataLoaderTask {
+            func cancel() {
+                
+            }
+        }
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            messages.append((url, completion))
+            return Task()
+        }
+    }
+}
