0bed281e8c1043ca39adb4037ee6527aab6ae36c
Vladislav Todorov <it.vtodorov@gmail.com>
2024-01-07 15:06:41 -0500

    Move FeedImageDataLoaderWithFallbackComposite to production

---
+++ EssentialApp/EssentialApp/FeedImageDataLoaderWithFallBackComposite.swift
@@ -0,0 +1,43 @@
+//
+//  FeedImageDataLoaderWithFallBackComposite.swift
+//  EssentialApp
+//
+//  Created by Fenominall on 07.01.2024.
+//
+
+import Foundation
+import EssentialFeed_2023
+
+public final class FeedImageDataLoaderWithFallBackComposite: FeedImageDataLoader {
+    private let primary: FeedImageDataLoader
+    private let fallback: FeedImageDataLoader
+    
+    private class TaskWrapper: FeedImageDataLoaderTask {
+        var wrapped: FeedImageDataLoaderTask?
+        
+        func cancel() {
+            wrapped?.cancel()
+        }
+    }
+    
+    public init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = TaskWrapper()
+        task.wrapped = primary.loadImageData(from: url) { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+            case .failure:
+                task.wrapped = self?.fallback
+                    .loadImageData(
+                        from: url,
+                        completion: completion)
+            }
+        }
+        return task
+    }
+}
