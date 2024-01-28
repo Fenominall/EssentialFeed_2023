82fb525aa50228490e4aa4d209d270db80c2ce89
Vladislav Todorov <it.vtodorov@gmail.com>
2024-01-28 15:25:42 +0300

    Add ImageComment data model

---
+++ EssentialFeed_2023/EssentialFeed_2023/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssentialFeed_2023
+//
+//  Created by Владислав Тодоров on 28.01.2024.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
