0866f068de8091c3d70db70a82c263c33ccc61e7
Vladislav Todorov <it.vtodorov@gmail.com>
2024-02-04 12:53:39 +0300

    Rename and move ResourceLoadingView and ViewModel to Shared module

---
+++ EssentialFeed_2023/EssentialFeed_2023/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialFeed_2023
+//
+//  Created by Владислав Тодоров on 04.02.2024.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
