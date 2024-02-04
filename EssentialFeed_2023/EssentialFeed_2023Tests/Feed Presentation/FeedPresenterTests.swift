9d076b382fd635842230dea1950721e5a76829fc
Vladislav Todorov <it.vtodorov@gmail.com>
2024-02-04 13:32:52 +0300

    Inline param

--- EssentialFeed_2023/EssentialFeed_2023Tests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed_2023/EssentialFeed_2023Tests/Feed Presentation/FeedPresenterTests.swift
@@ -11,7 +11,7 @@ import  EssentialFeed_2023
 final class FeedPresenterTests: XCTestCase {
     func test_title_isLocalized() {
         XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
-
+        
     }
     
     func test_map_createsViewModel() {
@@ -20,12 +20,13 @@ final class FeedPresenterTests: XCTestCase {
         let viewModel = FeedPresenter.map(feed)
         
         XCTAssertEqual(viewModel.feed, feed)
-
+        
     }
     
     // MARK: - Helpers
     
-    private func localized(_ key: String, table: String = "Feed", file: StaticString = #file, line: UInt = #line) -> String {
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
         let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
