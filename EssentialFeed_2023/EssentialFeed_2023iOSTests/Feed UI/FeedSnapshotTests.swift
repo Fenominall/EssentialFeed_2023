3a3685d23fc8c1f664357f0e84bcd0607ce566ea
Vladislav Todorov <it.vtodorov@gmail.com>
2024-02-09 18:53:42 +0300

    Exctract shared ListViewConroller shapshot tests

---
+++ EssentialFeed_2023/EssentialFeed_2023iOSTests/Feed UI/FeedSnapshotTests.swift
@@ -0,0 +1,113 @@
+//
+//  FeedSnapshotTests.swift
+//  EssentialFeed_2023iOSTests
+//
+//  Created by Владислав Тодоров on 14.01.2024.
+//
+
+import XCTest
+import EssentialFeed_2023iOS
+@testable import EssentialFeed_2023
+
+// Firstly we recoring the snapshots, that asserting that the record snapshots matches a new snapshot
+// Do not test the logic with snapshot tests, use unit and integration tests
+class FeedSnapshotTests: XCTestCase {
+
+    func test_feedWithContent() {
+        let sut = makeSUT()
+
+        sut.display(feedWithContent())
+
+        record(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "FEED_WITH_CONTENT_light")
+        record(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "FEED_WITH_CONTENT_dark")
+    }
+    
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+
+        sut.display(feedWithFailedImageLoading())
+
+        record(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
+        record(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+
+    private func feedWithContent() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: nil,
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: nil
+            ),
+            ImageStub(
+                description: nil,
+                location: "Garth Pier",
+                image: nil
+            )
+        ]
+    }
+}
+
+private extension ListViewController {
+    func display(_ stubs: [ImageStub]) {
+        let cells: [FeedImageCellController] = stubs.map { stub in
+            let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub)
+            stub.controller = cellController
+            return cellController
+        }
+
+        display(cells)
+    }
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+    let viewModel: FeedImageViewModel
+    let image: UIImage?
+    weak var controller: FeedImageCellController?
+
+    init(description: String?, location: String?, image: UIImage?) {
+        self.viewModel = FeedImageViewModel(
+            description: description,
+            location: location)
+        self.image = image
+    }
+
+    func didRequestImage() {
+        controller?.display(ResourceLoadingViewModel(isLoading: false))
+        
+        if let image = image {
+            controller?.display(image)
+            controller?.display(ResourceErrorViewModel(message: .none))
+        } else {
+            controller?.display(ResourceErrorViewModel(message: "any"))
+        }
+    }
+
+    func didCancelImageRequest() {}
+}
---
+++ EssentialFeed_2023/EssentialFeed_2023iOSTests/Shared UI/ListSnapshotTests.swift
@@ -0,0 +1,47 @@
+//
+//  ListSnapshotTests.swift
+//  EssentialFeed_2023iOSTests
+//
+//  Created by Владислав Тодоров on 09.02.2024.
+//
+
+import XCTest
+import EssentialFeed_2023iOS
+@testable import EssentialFeed_2023
+
+class ListSnapshotTests: XCTestCase {
+    func test_emptyFeed() {
+        let sut = makeSUT()
+        
+        sut.display(emptyList())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "EMPTY_LIST_light")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "EMPTY_LIST_dark")
+    }
+    
+    
+    func test_listWithErrorMessage() {
+        let sut = makeSUT()
+
+        sut.display(.error(message: "There is a\nmulti-line\nerror message"))
+        
+        record(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        record(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+    }
+    
+    
+    // MARK: - Helpers
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func emptyList() -> [CellController] {
+        return []
+    }
+}
